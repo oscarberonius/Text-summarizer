c6842e8102b3ba2e2be43b13a39596c80a786d88
oscarberonius <oscar.beronius@gmail.com>
2018-12-20 15:47:32 +0100

    Rough shell of pipeline

---
+++ src/generate_summary.py
@@ -0,0 +1,37 @@
+from '../models' import Extractive, Abstractive
+from '../utils' import DataProcessor
+import sys
+
+class Pipeline:
+
+    def __init__(self, data_path='../data/cluster', query=None, save_path='../data/summary.txt'): 
+        data_processor = DataProcessor()
+        self.data = data_processor.process_data(data_path)
+        self.query = query
+        self.save_path = save_path
+        self.extractive_summarizer = Extractive()
+        self.abstractive_worder = Abstractive()
+
+    def evaluate_cluster(self):
+        self.extractive_summary = self.extractive_summarizer.summarize(self.data, self.query)
+        self.abstractive_rewording = self.abstractive_worder.reword(extractive_summary)
+
+    def generate_summary(self):
+        formatted_summary = self.format(self.abstractive_rewording)
+        self.save(formatted)
+
+    def format(self, summary):
+        #Perform formatting if needed
+        self.save(summary)
+
+    def save(self, summary):
+        with open(self.path, 'w') as f:
+            for word in summary:
+                f.write(word)
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    pipeline = Pipeline(args)
+    pipeline.evaluate_cluster()
+    pipeline.generate_summary()
