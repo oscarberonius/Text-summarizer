e3ab2bf2d70a1eeb7f5ea06884b59036f01ec66d
oscarberonius <oscar.beronius@gmail.com>
2018-12-21 16:20:52 +0100

    Add some quiddity processing.

--- src/utils/data_processing.py
+++ src/utils/data_processing.py
@@ -1,6 +1,23 @@
+import json
+
 class DataProcessor:
     def __init__(self):
         pass
-    
+
     def process_data(self, path):
-        return "processed"
+        quiddities = self.process_search_result(path)
+        text,ingress = self.get_text_ingress(quiddities[0]['quiddity'])
+
+    def process_search_result(self, path):
+        with open(path, 'r') as fp:
+             obj = json.load(fp)
+        
+        quiddities = obj['views']['list']['results']
+
+        return quiddities
+
+    def get_text_ingress(self, dictionary):
+        text = dictionary['body']['content']['text']
+        ingress = dictionary['body']['ingress']['text']
+
+        return text, ingress
