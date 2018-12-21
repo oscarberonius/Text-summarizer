61adefe1a576a88f9bbc525010a80096b9799dac
oscarberonius <oscar.beronius@gmail.com>
2018-12-21 16:58:32 +0100

    Retrieve all ingresses and texts from file.

--- src/utils/data_processing.py
+++ src/utils/data_processing.py
@@ -5,16 +5,22 @@ class DataProcessor:
         pass
 
     def process_data(self, path):
-        quiddities = self.process_search_result(path)
-        text,ingress = self.get_text_ingress(quiddities[0]['quiddity'])
+        text_ingress_list = self.get_text_ingress_list(path)
+        print(text_ingress_list)
 
-    def process_search_result(self, path):
+    def get_text_ingress_list(self, path):
+        
         with open(path, 'r') as fp:
              obj = json.load(fp)
-        
-        quiddities = obj['views']['list']['results']
 
-        return quiddities
+        quiddities = obj['views']['list']['results']
+        text_ingress_list = []
+        for quiddity in quiddities:
+            text,ingress = self.get_text_ingress(quiddity['quiddity'])
+            res = dict({'text': text, 'ingress': ingress})
+            text_ingress_list.append(res)
+
+        return text_ingress_list
 
     def get_text_ingress(self, dictionary):
         text = dictionary['body']['content']['text']
