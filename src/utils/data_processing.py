8fa35560804035ac2bfb19d03f354924321c5793
oscarberonius <oscar.beronius@gmail.com>
2018-12-31 15:20:02 +0100

    Process tar file and extract texts and ingresses from the containing messages.

--- src/utils/data_processing.py
+++ src/utils/data_processing.py
@@ -12,13 +12,27 @@ class DataProcessor:
         else:
             text_ingress_list = self.read_data_folder(path)
 
-    def read_data_folder(self, path):     
-        with open(path, 'r') as outer:
-            for f in outer:
-                if tarfile.is_tarfile(f):
-                    innerfolder = tarfile.open(f).extractall()
+    def read_data_folder(self, path):  
+        
+        tar = tarfile.open(path+'/02d8cf54-a762-46a8-9251-944f3d43d0b2_00001_processed.tar.gz')
+        text_ingress_list = self.process_messages(tar)
+        tar.close()
+        # Untar all tarred folders in path
+
+    def process_messages(self, tar):
+        text_ingress_list = []
+
+        # Extract messages and retrieve texts and ingresses as dicts.
+        for member in tar.getmembers():
+            fileobj = tar.extractfile(member)
+            message = json.load(fileobj)
+            text, ingress = self.get_text_ingress(message)
+            res = dict({'text': text, 'ingress': ingress})
+            text_ingress_list.append(res)
+
+        return text_ingress_list
                 
-    def get_text_ingress_list(self, path):       
+    def get_text_ingress_list(self, path):
         with open(path, 'r') as fp:
              obj = json.load(fp)
 
