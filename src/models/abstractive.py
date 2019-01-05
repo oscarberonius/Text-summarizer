b483952115631fe3245f6e40ab0b28a8e1cd060c
oscarberonius <oscar.beronius@gmail.com>
2019-01-05 09:16:28 +0100

    Add seq2seq summarizer.

--- src/models/abstractive.py
+++ src/models/abstractive.py
@@ -1,7 +1,188 @@
+from __future__ import print_function
+
+from keras.models import Model
+from keras.layers import Input, LSTM, Dense
+import numpy as np
+import json
+from .ind_rnn import IndRNN
+from os.path import dirname
+
 class Abstractive:
-    def __init__(self):
-        pass
+    def __init__(self, data=None):
+        data = data # list(dict({text, ingress}))
+        self.batch_size = 64  # Batch size for training.
+        self.epochs = 100  # Number of epochs to train for.
+        self.latent_dim = 256  # Latent dimensionality of the encoding space.
+        self.num_samples = 10000  # Number of samples to train on.
+
+        input_characters = set()
+        target_characters = set()
+        input_texts = []
+        target_texts = []
+
+        counter = 0
+        for dictionary in data[0:self.num_samples]:
+            counter+=1
+
+            input_texts.append(dictionary['text'])
+            target_texts.append(dictionary['ingress'])
+
+            if counter%1000 == 0:
+                print(counter)
+            for c in dictionary['text']:
+                input_characters.add(c)
+            for c in dictionary['ingress']:
+                target_characters.add(c)
+
+        del data
+        input_characters = sorted(list(input_characters))
+        target_characters = sorted(list(target_characters))
+        num_encoder_tokens = len(input_characters)
+        num_decoder_tokens = len(target_characters)
+        max_encoder_seq_length = max([len(txt) for txt in input_texts])
+        max_decoder_seq_length = max([len(txt) for txt in target_texts])
+
+
+        print('Number of samples:', len(input_texts))
+        print('Number of unique input tokens:', num_encoder_tokens)
+        print('Number of unique output tokens:', num_decoder_tokens)
+        print('Max sequence length for inputs:', max_encoder_seq_length)
+        print('Max sequence length for outputs:', max_decoder_seq_length)
+
+        input_token_index = dict(
+            [(char, i) for i, char in enumerate(input_characters)])
+        target_token_index = dict(
+            [(char, i) for i, char in enumerate(target_characters)])
+
+        encoder_input_data = np.zeros(
+            (len(input_texts), max_encoder_seq_length, num_encoder_tokens),
+            dtype='int8')
+        decoder_input_data = np.zeros(
+            (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+            dtype='int8')
+        decoder_target_data = np.zeros(
+            (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+            dtype='int8')
+
+        for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
+            for t, char in enumerate(input_text):
+                encoder_input_data[i, t, input_token_index[char]] = 1.
+            for t, char in enumerate(target_text):
+                # decoder_target_data is ahead of decoder_input_data by one timestep
+                decoder_input_data[i, t, target_token_index[char]] = 1.
+                if t > 0:
+                    # decoder_target_data will be ahead by one timestep
+                    # and will not include the start character.
+                    decoder_target_data[i, t - 1, target_token_index[char]] = 1.
+
+        # Define an input sequence and process it.
+        encoder_inputs = Input(shape=(max_encoder_seq_length, num_encoder_tokens))
+        encoder1 = IndRNN(self.latent_dim, return_state=True, return_sequences=True)
+        encoder1_outputs, encoder1_states = encoder1(encoder_inputs)
+        encoder2 = IndRNN(self.latent_dim, return_state=True)
+        encoder_outputs, encoder_states = encoder2(encoder1_outputs)
+        # We discard `encoder_outputs` and only keep the states.
+
+        # Set up the decoder, using `encoder_states` as initial state.
+        decoder_inputs = Input(shape=(None, num_decoder_tokens))#max_decoder_seq_length, num_decoder_tokens))
+        # We set up our decoder to return full output sequences,
+        # and to return internal states as well. We don't use the
+        # return states in the training model, but we will use them in inference.
+        decoder_indrnn = IndRNN(self.latent_dim, return_sequences=True, return_state=True)
+        decoder_outputs, _ = decoder_indrnn(decoder_inputs,
+                                            initial_state=encoder_states)
+        decoder_dense = Dense(num_decoder_tokens, activation='softmax')
+        decoder_outputs = decoder_dense(decoder_outputs)
+
+        # Define the model that will turn
+        # `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
+        model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
+        # Run training
+        model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+        model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
+                batch_size=self.batch_size,
+                epochs=self.epochs,
+                validation_split=0.2)
+        # Save model
+        model.save('s2s.h5')
+
+        # Next: inference mode (sampling).
+        # Here's the drill:
+        # 1) encode input and retrieve initial decoder state
+        # 2) run one step of decoder with this initial state
+        # and a "start of sequence" token as target.
+        # Output will be the next target token
+        # 3) Repeat with the current target token and current states
+
+        # Define sampling models
+        encoder_model = Model(encoder_inputs, encoder_states)
+
+        decoder_state_input = Input(shape=(self.latent_dim,))
+        decoder_outputs, decoder_state = decoder_indrnn(
+            decoder_inputs, initial_state=decoder_state_input)
+        decoder_outputs = decoder_dense(decoder_outputs)
+        decoder_model = Model(
+            [decoder_inputs] + [decoder_state_input],
+            [decoder_outputs] + [decoder_state])
+
+        # Reverse-lookup token index to decode sequences back to
+        # something readable.
+        reverse_input_char_index = dict(
+            (i, char) for char, i in input_token_index.items())
+        reverse_target_char_index = dict(
+            (i, char) for char, i in target_token_index.items())
+
+
+        def decode_sequence(input_seq):
+            # Encode the input as state vectors.
+            states_value = encoder_model.predict(input_seq)
+
+            # Generate empty target sequence of length 1.
+            target_seq = np.zeros((1, 1, num_decoder_tokens))
+            # Populate the first character of target sequence with the start character.
+            target_seq[0, 0, target_token_index['\t']] = 1.
+
+            # Sampling loop for a batch of sequences
+            # (to simplify, here we assume a batch of size 1).
+            stop_condition = False
+            decoded_sentence = ''
+            while not stop_condition:
+                output_tokens, state = decoder_model.predict(
+                    [target_seq] + [states_value])
+
+                # Sample a token
+                sampled_token_index = np.argmax(output_tokens[0, -1, :])
+                sampled_char = reverse_target_char_index[sampled_token_index]
+                decoded_sentence += sampled_char
+
+                # Exit condition: either hit max length
+                # or find stop character.
+                if (sampled_char == '\n' or
+                len(decoded_sentence) > max_decoder_seq_length):
+                    stop_condition = True
+
+                # Update the target sequence (of length 1).
+                target_seq = np.zeros((1, 1, num_decoder_tokens))
+                target_seq[0, 0, sampled_token_index] = 1.
+
+                # Update states
+                states_value = state#[h, c]
+
+            return decoded_sentence
+
+
+        for seq_index in range(100):
+            # Take one sequence (part of the training set)
+            # for trying out decoding.
+            input_seq = encoder_input_data[seq_index: seq_index + 1]
+            decoded_sentence = decode_sequence(input_seq)
+            print('-')
+            print('Input sentence:', input_texts[seq_index])
+            print('Decoded sentence:', decoded_sentence)
+
+
+
 
     def reword(self, text):
-        ret = ["reworded", "summary"]
-        return ret
+        pass
